8afef036b2d3299d935377b0df49e51930cbe8e5
Ashwattha Phatak <ashwatthap@gmail.com>
2024-09-09 00:38:32 +0000

    Modified debugginh.py and added rand.py, added pylint json in post_traces for logging.

--- debugging.py
+++ debugging.py
@@ -1,19 +1,37 @@
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        lowestIndex = i
-        for j in range(n):
-            if arr[j] < arr[lowestIndex]:
-                lowestIndex = j
+import rand
 
-        arr[lowestIndex] = arr[i]
-        arr[i] = arr[lowestIndex]
-        n = n - 1
 
-    return arr
+def mergeSort(arr):
+    if len(arr) == 1:
+        return arr
 
-arr_in = [5, 3, 2, 1, 8, 10, 11, 9, 23]
+    half = len(arr) // 2
 
-arr_out = selectionSort(arr_in)
+    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+
+
+def recombine(leftArr, rightArr):
+    leftIndex = 0
+    rightIndex = 0
+    mergeArr = [None] * (len(leftArr) + len(rightArr))
+    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
+        if leftArr[leftIndex] < rightArr[rightIndex]:
+            rightIndex += 1
+            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+        else:
+            leftIndex += 1
+            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+
+    for i in range(rightIndex, len(rightArr)):
+        mergeArr[leftIndex + rightIndex] = rightArr[i]
+
+    for i in range(leftIndex, len(leftArr)):
+        mergeArr[leftIndex + rightIndex] = leftArr[i]
+
+    return mergeArr
+
+
+arr = rand.random_array([None] * 20)
+arr_out = mergeSort(arr)
 
 print(arr_out)
